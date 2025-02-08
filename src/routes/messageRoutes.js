bf7529cbd8edcb3799fc30177eaf691f57135fe4
Felipe Santos <lipesouzasanto@gmail.com>
2025-02-08 17:24:15 -0300

    Fix> Escrita na importação

--- src/routes/messageRoutes.js
+++ src/routes/messageRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const MessageController = require("../controllers/MessageController")
+const MessageController = require("../controllers/messageController")
 
 const router = express.Router()
 
