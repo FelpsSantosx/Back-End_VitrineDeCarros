70fa170dfe1cc80d79379ef98d1114c95f9af1cb
Felipe Santos <lipesouzasanto@gmail.com>
2025-06-18 00:26:45 -0300

    commit rota*2

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 const origins =
-  process.env.NODE_ENV === "development" // alterar para "production" quando for para em produção
+  process.env.NODE_ENV === "production" // alterar para "production" quando for para em produção
     ? [process.env.PROD_URL]
     : [process.env.DEV_URL];
 
